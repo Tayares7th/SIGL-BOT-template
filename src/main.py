e2ce4dcaba650039a36b917630f2619f07d91417
aksel kuru <aksel.kuru@epita.fr>
2021-09-29 19:45:35 +0200

    test

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ bot = commands.Bot(
     case_insensitive=True  # Commands aren't case-sensitive
 )
 
-bot.author_id = 0000000  # Change to your discord id!!!
+bot.author_id = 892820234992504852  # Change to your discord id!!!
 
 @bot.event
 async def on_ready():  # When the bot is ready
@@ -17,5 +17,5 @@ async def on_ready():  # When the bot is ready
 async def pong(ctx):
     await ctx.send('pong')
 
-token = "<MY_SUPER_TOKEN>"
+token = "ODkyODI0NTgxNzUxMDYyNTI4.YVShsA.41dBuUTrViEd0rn-uL7cWkXazdM"
 bot.run(token)  # Starts the bot
